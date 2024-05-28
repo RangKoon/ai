7a3015cb28e81523c309abeaae3c56ac327d4141
hydrationn <suhwaparkk1031@gmail.com>
2024-05-28 12:09:39 +0900

    first commit

---
+++ src/test/java/com/ai/yummyheap/YummyheapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.yummyheap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyheapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
